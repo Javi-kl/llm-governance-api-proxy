3e31fbd395585f17d2da4504c483b1fcdb30ee4a
javi-kl <203809377+Javi-kl@users.noreply.github.com>
2026-07-31 10:12:00 +0200

    feat: agregar script de cli para recibir input y mostrar la apikey

---
+++ scripts/create_api_key.py
@@ -0,0 +1,21 @@
+from app.db.database import get_db_context
+from app.services.api_keys import create_for_user
+from app.core import exceptions
+
+
+def main() -> None:
+    username = input("Username: ")
+    key_name = input("Nombre de la API key: ")
+
+    try:
+        with get_db_context() as db:
+            raw_key = create_for_user(
+                username=username,
+                key_name=key_name,
+                db=db,
+            )
+
+            print(f"API key: {raw_key}")
+
+    except exceptions.UserNotFoundError as exc:
+        print(exc.message)
