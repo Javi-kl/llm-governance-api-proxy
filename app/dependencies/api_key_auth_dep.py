685de7c7dff445fec1e12f357666c85cad8ed304
javi-kl <203809377+Javi-kl@users.noreply.github.com>
2026-08-03 08:46:49 +0200

    feat(openaicomp) dependencia de autorización corregida.

--- app/dependencies/api_key_auth_dep.py
+++ app/dependencies/api_key_auth_dep.py
@@ -1,16 +1,29 @@
-from fastapi.security import HTTPAuthorizationCredentials
+from typing import Annotated
+
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.db.models.user import User
 from app.core import exceptions, security
 from app.repositories.api_keys import get_by_hash
+from app.db.database import get_db
+
+bearer_scheme = HTTPBearer(auto_error=False)
 
 
 def api_key_auth_dep(
-    credentials: HTTPAuthorizationCredentials,
-    db: Session,
+    bearer_credentials: Annotated[
+        HTTPAuthorizationCredentials | None,
+        Depends(bearer_scheme),
+    ],
+    db: Annotated[Session, Depends(get_db)],
 ) -> User:
-    raw_key = credentials.credentials
+
+    if bearer_credentials is None:
+        raise exceptions.InvalidCredentialsError()
+
+    raw_key = bearer_credentials.credentials
 
     if not raw_key.startswith("lgp_"):
         raise exceptions.InvalidCredentialsError()
